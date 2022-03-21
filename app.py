7712abff652ef824b498ab5f2c07254ed38d0580
Luke Ingram <lukeingram01@gmail.com>
2022-03-21 12:30:54 -0400

    Restructuring

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template,request,redirect,url_for,abort,send_from_directory
+from flask import Flask, render_template,request,redirect,url_for,abort,send_from_directory,flash
 from werkzeug.utils import secure_filename 
 import os 
 from validate import *
 
 
 app = Flask(__name__)
+
 app.config["MAX_CONTENT_LENGTH"] = 10**7
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg','.png','.jpeg']
 app.config['UPLOAD_PATH'] = './uploads'
@@ -24,13 +25,14 @@ def upload_file():
     if fname != '': 
         file_ext = os.path.splitext(fname)[1]
         if file_ext not in app.config['UPLOAD_EXTENSIONS'] or file_ext != validate_image(file.stream):
-            abort(400)
+            flash("Unsupported File. Supported types: (png, jpg, jpeg)")
+            return redirect(url_for('index'))
         file.save(os.path.join(app.config["UPLOAD_PATH"],fname))
     return redirect(url_for('index'))
 
 
 @app.route('/uploads/<filename>')
-def upload(filename):
+def display(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 if __name__ == "__main__": 
